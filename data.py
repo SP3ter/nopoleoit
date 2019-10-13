9f30c5087ba6c7df41cce2dccd38cd67f5a6d250
SP3ter <56481721+SP3ter@users.noreply.github.com>
2019-10-13 18:39:40 +0200

    Added length of user list

--- data.py
+++ data.py
@@ -1,19 +1,19 @@
 while(True): 
+ user_input_length=0
+ while(user_input_length<=2 or user_input_length>=10000):
+  user_input_length=int(input("введите длину массива в диапозоне от 2 до 10000\n"))
  user_input=input("введите числа") 
  user_input=user_input.replace(" ","").replace(",","") 
  data=list(user_input) 
- 
+ data=data[:user_input_length]
  divided_3=[] 
  others=[] 
- 
- if len(data) >=2 and 10000 >= len(data): 
-  for num in data: 
-   if int(num)%3 == 0: 
+  
+ for num in data: 
+  if int(num)%3 == 0: 
    divided_3.append(num) 
   else: 
    others.append(num) 
- else: 
- print("введите число в интервале от 2 до 10000") 
  divided_3.sort() 
  others.sort() 
  result=divided_3+others 
